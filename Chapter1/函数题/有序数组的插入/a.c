675a137c38cd78581ba6b53b53145c297c34ade7
Ding Zhan <dingzh@jit.edu.cn>
2020-03-23 20:49:39 +0800

    Comment

--- Chapter1/函数题/有序数组的插入/a.c
+++ Chapter1/函数题/有序数组的插入/a.c
@@ -1,12 +1,15 @@
-
+//通过二分查找法找到合适的插入位置
 bool Insert( List L, ElementType X )
 {
+	//lastValidPos表示一个合适的插入位置
     int begin=0, end= L->Last, mid, i, lastValidPos=-1;
+	//线性表已满
     if(L->Last==MAXSIZE-1)
       return false;
     while(begin<=end)
     {
         mid = begin+(end-begin)/2;
+		//该元素已经在排序好的数组中存在，则返回false
         if(X==L->Data[mid])
         {
             return false;
@@ -21,14 +24,17 @@ bool Insert( List L, ElementType X )
             lastValidPos = mid;
         }
     }
+	//如果还没有合适的插入位置，则表示要添加到尾部
     if(lastValidPos==-1)
         lastValidPos = L->Last+1;
-
+	//从后往前移动数组内容
     for(i=L->Last; i>=lastValidPos; --i)
     {
         L->Data[i+1] = L->Data[i];
     }
+	//插入新值
     L->Data[lastValidPos]=X;
+	//数组最大下标加1
     ++L->Last;
     return true;
 }
